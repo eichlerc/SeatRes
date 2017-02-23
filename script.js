dc43d2eb5689669448a410c797b812b2e4740656
Adam Lezotte <adam@lezotte.me>
2017-02-22 20:42:51 -0500

    jquery

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     
+    $('form').hide();
+    
     $('.seat img').click(function(){
-        $(this).attr('src', 'images/seat.svg');
+      $(this).attr('src', 'images/seat.svg');
+      $('form').show();
+    });
+    
+    $('button').click(function(){
+      $('form').hide();
     });
     
 });
