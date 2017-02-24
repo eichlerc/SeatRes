b5504c10ba13e5a27d966d30abac16005c29304c
Adam Lezotte <adam@lezotte.me>
2017-02-23 19:32:53 -0500

    array of objects

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ $(document).ready(function(){
     
     $('form').hide();
     
-    $('.seat img').click(function(){
-      $(this).attr('src', 'images/seat.svg');
+    var $id;
+    var reservedSeats = [];
+    
+    $('.seat').click(function(){
+      $('img',this).attr('src', 'images/seat.svg');
+      $(this).addClass('reserved').off();
+      $id = $(this).attr('id');
       $('form').show();
     });
     
     $('button').click(function(){
+      //$('form').reset();
       event.preventDefault();
+      //console.log($id);
+      //console.log($('#name').prop('value'));
+      reservedSeats.push({seat: $id, name: $('#name').prop('value')});
       $('form').hide();
+      for (i = 0; i < reservedSeats.length; i++){
+        console.log(reservedSeats[i].name);
+        console.log(reservedSeats[i].seat);
+      }
     });
+    
+
     
 });
